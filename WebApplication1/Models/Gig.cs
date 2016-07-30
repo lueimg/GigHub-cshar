b218ca03058d4e3e18c64e342db3973afbd0d3a5
luis <luis@DESKTOP-E2CP9J0>
2016-07-30 05:05:21 -0700

    Add project files.

---
+++ WebApplication1/Models/Gig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Artist { get; set; }
+        [Required]
+        public string ArtistId { get; set; }
+
+        public DateTime Datetime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
+    }
+
+    public class Genre
+    {
+        public byte id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+    }
+}
